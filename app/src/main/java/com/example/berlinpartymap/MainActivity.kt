a031357b18f87b2fe16d154d85cd7072de3911ae
AT-Ade <at.adeyinka@gmail.com>
2026-05-18 15:19:57 +0200

    Navigation eingerichtet (mit bottombar), SavedEventsScreen.kt angelegt, PartyHistoryScreen.kt angelegt

--- app/src/main/java/com/example/berlinpartymap/MainActivity.kt
+++ app/src/main/java/com/example/berlinpartymap/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.berlinpartymap.ui.AppStart
 import com.example.berlinpartymap.ui.map.MapScreen
 import com.example.berlinpartymap.ui.theme.MyApplicationTheme
 import org.osmdroid.config.Configuration
@@ -32,9 +33,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MapScreen(
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    AppStart(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
---
+++ app/src/main/java/com/example/berlinpartymap/data/enums/TabItems.kt
@@ -0,0 +1,21 @@
+package com.example.berlinpartymap.data.enums
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.History
+import androidx.compose.material.icons.rounded.Map
+import androidx.compose.material.icons.rounded.Star
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.example.berlinpartymap.ui.HistoryRoute
+import com.example.berlinpartymap.ui.MapRoute
+import com.example.berlinpartymap.ui.SavedRoute
+
+enum class TabItem(
+    val route: Any,
+    val tabTitle: String,
+    val tabIcon: ImageVector
+) {
+    MAP(MapRoute, "Home", Icons.Rounded.Map),
+    FAVOURITES(SavedRoute, "Favourites", Icons.Rounded.Star),
+    HISTORY(HistoryRoute, "History", Icons.Rounded.History),
+    //...
+}
